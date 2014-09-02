3089c23e29ea8c559b99487fcc995f8e90f3e33e
dshamis317 <dshamis317@gmail.com>
2014-09-02 09:43:29 -0400

    added slide animations for team bio's

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,15 @@
+function displayBios() {
+  $('#ray .bio').slideDown();
+
+  $('.personnel img').on('click', function() {
+    var thisBio = this.parentElement.lastElementChild
+    $('.bio').slideUp();
+    if ($(thisBio).is(":hidden")) {
+     $(thisBio).slideDown();
+   }
+ })
+}
+
+$(function() {
+  displayBios();
+})
